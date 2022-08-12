74a665b12eeae520900c62f7e104da96992203b1
gbloch <gaetan.bloch@gmail.com>
2022-08-12 11:10:03 +0200

    feat(mower): add command line handling to provide a file and then get the result in the console

---
+++ script/mowing.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Java 17 must be installed
+java -jar ./mower-1.0.jar ./sample.txt
--- src/main/java/io/gbloch/mower/Main.java
+++ src/main/java/io/gbloch/mower/Main.java
@@ -1,8 +1,25 @@
 package io.gbloch.mower;
 
+import io.gbloch.mower.handler.MowingHandler;
+import java.io.IOException;
+
 public class Main {
 
   public static void main(String[] args) {
-    // todo handle command line execution
+    if (args.length == 0) {
+      System.err.println("Error: You must provide a field configuration file.");
+      return;
+    }
+
+    // The first argument of the command line is the path
+    // to the field configuration file
+    String filePath = args[0];
+    System.out.println("File path: " + filePath);
+    MowingHandler mowingHandler = new MowingHandler(filePath);
+    try {
+      System.out.println(mowingHandler.handle());
+    } catch (IOException e) {
+      System.err.println("Error while calculating the mower positions: " + e.getMessage());
+    }
   }
 }
